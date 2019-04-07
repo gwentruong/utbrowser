3949c9f96ca8cd95cc0a7b4f53ff42abbf328b97
Uyen Truong <haiuyentruong@gmail.com>
2019-04-07 11:41:17 +0300

    Show text by pango instead of cairo

--- sdl2-test/test.c
+++ sdl2-test/test.c
@@ -1,7 +1,9 @@
 // Using SDL and standard IO
 #include <SDL.h>
 #include <cairo.h>
+#include <pango/pangocairo.h>
 #include <stdio.h>
+
 
 // Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -13,6 +15,8 @@ int main(int argc, char **argv)
     SDL_Surface     *sdlsurf   = NULL;
     cairo_surface_t *cairosurf = NULL;
     cairo_t         *cr        = NULL;
+    PangoLayout     *layout;
+    PangoFontDescription *font_description;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -33,20 +37,32 @@ int main(int argc, char **argv)
                                                             sdlsurf->pitch);
             cr        = cairo_create(cairosurf);
 
-            // Fill the surface white
-            // SDL_FillRect(sdlsurf, NULL, SDL_MapRGB(sdlsurf->format,
-                         // 0xFF, 0xFF, 0xFF));
-
             cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
             cairo_paint(cr);
 
-            cairo_select_font_face(cr, "serif",
-                                   CAIRO_FONT_SLANT_NORMAL,
-                                   CAIRO_FONT_WEIGHT_BOLD);
-            cairo_set_font_size(cr, 32.0);
+            // cairo_select_font_face(cr, "serif",
+            //                        CAIRO_FONT_SLANT_NORMAL,
+            //                        CAIRO_FONT_WEIGHT_BOLD);
+            // cairo_set_font_size(cr, 32.0);
+            // cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+            // cairo_move_to(cr, 10.0, 50.0);
+            // cairo_show_text(cr, "Hello!... Yifan");
+
+            font_description = pango_font_description_new();
+            pango_font_description_set_family(font_description, "serif");
+            pango_font_description_set_weight(font_description, PANGO_WEIGHT_BOLD);
+            pango_font_description_set_absolute_size(font_description, 32 * PANGO_SCALE);
+
+            layout = pango_cairo_create_layout(cr);
+            pango_layout_set_font_description(layout, font_description);
+            pango_layout_set_text(layout, "Hello, world", -1);
+
             cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
-            cairo_move_to(cr, 10.0, 50.0);
-            cairo_show_text(cr, "Hello!... Yifan");
+            cairo_move_to(cr, 10.0, 40.0);
+            pango_cairo_show_layout(cr, layout);
+
+            g_object_unref(layout);
+            pango_font_description_free(font_description);
 
             SDL_UpdateWindowSurface(window);
 
