b69e94f32fa6b7b5c95f63a0c248912a47c4c850
Uyen Truong <haiuyentruong@gmail.com>
2019-04-14 11:34:09 +0300

    Modify error print

--- http.c
+++ http.c
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        perror("Usage: send_file <IP address>");
+        perror("Usage: ./http <IP address>");
         return -1;
     }
 
