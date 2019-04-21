58c4978a8b3b6e697f956f8e81cd23d3c9565df6
Uyen Truong <haiuyentruong@gmail.com>
2019-04-21 15:51:24 +0300

    Move macro definitions to header

--- utbrowser.h
+++ utbrowser.h
@@ -1,6 +1,9 @@
 #ifndef __UTBROWSER_H__
 #define __UTBROWSER_H__
 
+#define PORT "80"
+#define MAX_LEN 8192
+
 void  remove_string (char text[], int index, int rm_length);
 const char *cleantext(GumboNode* node);
 
